7c28633d120694566428046f841d9418d862a74e
Taha991 <tahamohamedman@gmail.com>
2023-05-03 15:22:53 +0200

    Adding Specification Class

---
+++ Core/Specifications/BaseSpecification.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specifications
+{
+    internal class BaseSpecification
+    {
+    }
+}
