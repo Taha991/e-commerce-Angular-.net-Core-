ec4286d0e5cbaab77de3f1cd0dea0a10ef06ff5e
Taha991 <tahamohamedman@gmail.com>
2023-05-12 15:03:29 +0200

    Adding a child component for the product items

--- Client/src/app/shop/shop.module.ts
+++ Client/src/app/shop/shop.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { ShopRoutingModule } from './shop-routing.module';
 import { ShopComponent } from './shop/shop.component';
+import { ProductItemComponent } from './product-item/product-item.component';
 
 
 @NgModule({
   declarations: [
-    ShopComponent
+    ShopComponent,
+    ProductItemComponent
   ],
   imports: [
     CommonModule,
