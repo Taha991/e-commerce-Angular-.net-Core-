ced66ab3b1681d45b7271b6df73b146b98a375dd
Taha991 <tahamohamedman@gmail.com>
2023-05-12 14:22:14 +0200

    Consuming services in the component

--- Client/src/app/shop/shop.service.ts
+++ Client/src/app/shop/shop.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pagination } from '../shared/models/paging';
+import { Product } from '../shared/models/product';
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class ShopService {
 
-  constructor() { }
+
+  constructor(private http:HttpClient) { }
+  baseUrl = "https://localhost:7275/api/"
+  getProducts ()
+  {
+    return this.http.get<Pagination<Product>>(this.baseUrl+'product')
+  }
+
+
 }
