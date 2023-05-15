1f6d21163d715ccc46734090d1ba0378484701ab
Taha991 <tahamohamedman@gmail.com>
2023-05-15 14:53:33 +0200

    Creating Additional Error Components

--- Client/src/app/app-routing.module.ts
+++ Client/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home/home.component';
 import { TestErrorComponent } from './core/test-error/test-error.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 
 const routes: Routes = [
   {path:'' , component:HomeComponent},
   {path:'test-error' , component:TestErrorComponent},
+  {path:'server-error' , component:ServerErrorComponent},
+  {path:'not-found'  , component:NotFoundComponent},
   {path:'shop' , loadChildren:()=>import('./shop/shop.module').then(m=>m.ShopModule)},
   {path:'**' , redirectTo:'' , pathMatch:'full'}
 ];
--- Client/src/app/core/core.module.ts
+++ Client/src/app/core/core.module.ts
@@ -4,10 +4,12 @@ import { CoreRoutingModule } from './core-routing.module';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { TestErrorComponent } from './test-error/test-error.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
-  declarations: [NavBarComponent, TestErrorComponent],
+  declarations: [NavBarComponent, TestErrorComponent, ServerErrorComponent, NotFoundComponent],
   imports: [
     CommonModule,
     CoreRoutingModule,
