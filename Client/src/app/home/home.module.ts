c512af0c673a120aedf17e5a586667432e8f1265
Taha991 <tahamohamedman@gmail.com>
2023-05-16 12:06:56 +0200

    Adding the hompe page content

--- Client/src/app/home/home.module.ts
+++ Client/src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    SharedModule
   ],
   exports:[HomeComponent]
 })
--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -5,7 +5,7 @@ import { SharedRoutingModule } from './shared-routing.module';
 import { PaginationModule } from 'ngx-bootstrap/pagination';
 import { PagingHeaderComponent } from './paging-header/paging-header.component';
 import { PagerComponent } from './pager/pager.component';
-
+import {CarouselModule} from 'ngx-bootstrap/carousel'
 
 @NgModule({
   declarations: [
@@ -15,11 +15,13 @@ import { PagerComponent } from './pager/pager.component';
   imports: [
     CommonModule,
     SharedRoutingModule,
-    PaginationModule.forRoot()
+    PaginationModule.forRoot(),
+    CarouselModule.forRoot()
   ],
   exports:[PaginationModule,
     PagingHeaderComponent,
-    PagerComponent
+    PagerComponent,
+    CarouselModule
   ]
 })
 export class SharedModule { }
