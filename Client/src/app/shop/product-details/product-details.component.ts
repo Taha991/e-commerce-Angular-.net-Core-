f8d2d7a17565921228df5895bdd936fe98112d61
Taha991 <tahamohamedman@gmail.com>
2023-05-16 11:04:49 +0200

    adding the breadcrumb service + style it

--- Client/src/app/shop/product-details/product-details.component.ts
+++ Client/src/app/shop/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component  , OnInit} from '@angular/core';
 import { ShopService } from '../shop.service';
 import { Product } from 'src/app/shared/models/product';
 import { ActivatedRoute } from '@angular/router';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-product-details',
@@ -11,7 +12,7 @@ import { ActivatedRoute } from '@angular/router';
 export class ProductDetailsComponent  implements OnInit{
 
   product?:Product;
-   constructor( private shopServices:ShopService , private activatedRoute: ActivatedRoute) {
+   constructor( private shopServices:ShopService , private activatedRoute: ActivatedRoute ,private bcService:BreadcrumbService) {
 
 
    }
@@ -25,7 +26,10 @@ export class ProductDetailsComponent  implements OnInit{
   {
     const id = this.activatedRoute.snapshot.paramMap.get('id');
     if(id) this.shopServices.getProduct(+id).subscribe({
-      next:product => this.product =product,
+      next:product => {this.product =product;
+        this.bcService.set('@productDetails' , product.name)
+
+      },
       error: error => console.log(error)
 
     });
