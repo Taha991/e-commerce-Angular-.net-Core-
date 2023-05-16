778f57faae06ab8a0b086b623361b579ded1a3eb
Taha991 <tahamohamedman@gmail.com>
2023-05-16 11:12:02 +0200

    setting the section header title from the breadcrumb service

--- Client/src/app/core/section-header/section-header.component.ts
+++ Client/src/app/core/section-header/section-header.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component  , OnInit} from '@angular/core';
+import { BreadcrumbService } from 'xng-breadcrumb';
 
 @Component({
   selector: 'app-section-header',
   templateUrl: './section-header.component.html',
   styleUrls: ['./section-header.component.scss']
 })
-export class SectionHeaderComponent {
+export class SectionHeaderComponent implements OnInit {
+
+
+  constructor(public bcService:BreadcrumbService) {
+
+
+  }
+  ngOnInit(): void {
+  }
 
 }
