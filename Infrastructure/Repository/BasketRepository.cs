0c0a3ec04e54d4d4d9b0d0fcac57099bc9e26752
Taha991 <tahamohamedman@gmail.com>
2023-05-17 13:27:55 +0200

    Adding a basket controller

--- Infrastructure/Repository/BasketRepository.cs
+++ Infrastructure/Repository/BasketRepository.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Core.Entities;
+using Core.Interfaces;
+using StackExchange.Redis;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Infrastructure.Repository
 {
-    internal class BasketRepository
+    public class BasketRepository : IBasketRepository
     {
+        private readonly IDatabase _database;
+        public BasketRepository(IConnectionMultiplexer redis)
+        {
+            _database = redis.GetDatabase();
+        }
+
+
+        public async  Task<bool> DeletBasketAsync(string basketId)
+        {
+            return await _database.KeyDeleteAsync(basketId);
+         }
+
+        public async Task<CustomerBasket> GetBasketAsync(string basketId)
+        {
+            var data = await _database.StringGetAsync(basketId);
+            return data.IsNullOrEmpty ? null : JsonSerializer.Deserialize<CustomerBasket>(data);
+        }
+
+        public async Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket)
+        {
+            var created = await _database.StringSetAsync(basket.Id , JsonSerializer.Serialize(basket), TimeSpan.FromDays(30));
+            if (!created) return null;
+            return await GetBasketAsync(basket.Id);
+        }
     }
 }
