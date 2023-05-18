54a2228c0db7fae82ef2e7735fecd87ee592ebd1
Taha991 <tahamohamedman@gmail.com>
2023-05-18 12:54:18 +0200

    Creating the basket types + installing cuid

---
+++ Client/src/app/shared/models/basket.ts
@@ -0,0 +1,23 @@
+import * as cuid from 'cuid'
+export interface BasketItem{
+
+  id:number;
+  productName:string;
+  price:number;
+  quantity:number;
+  pictureUrl:string;
+  brand:string;
+  type:string;
+
+}
+
+export interface Basket{
+  id:string;
+  items:BasketItem[];
+}
+
+export class Basket implements Basket
+{
+  id=cuid();
+  items: BasketItem[]=[];
+}
