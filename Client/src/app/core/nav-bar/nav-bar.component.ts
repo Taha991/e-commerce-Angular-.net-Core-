f6bf649a8f808a36f3e1419d02cb3129adf4755e
Taha991 <tahamohamedman@gmail.com>
2023-05-18 13:40:15 +0200

    Displaying the basket item count in the nav bar

--- Client/src/app/core/nav-bar/nav-bar.component.ts
+++ Client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component  , OnInit} from '@angular/core';
+import { BasketService } from 'src/app/basket/basket.service';
+import { BasketItem } from 'src/app/shared/models/basket';
 
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.scss']
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
 
+
+  constructor(public basketService:BasketService){}
+
+
+  ngOnInit(): void {
+  }
+
+  getCount(items:BasketItem[])
+  {
+    return items.reduce((sum, items)=> sum+items.quantity,0)
+  }
 }
