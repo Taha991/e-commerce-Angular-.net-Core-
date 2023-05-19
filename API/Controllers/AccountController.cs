401b97cc3452a7ed47e2f721784f7c1c995de2ea
Taha991 <tahamohamedman@gmail.com>
2023-05-19 15:59:29 +0200

    Adding registering a user

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -43,6 +43,28 @@ namespace API.Controllers
                 DisplayName = user.DisplayName,
             };
         }
+
+
+        [HttpPost("register")]
+
+        public async Task <ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            var user = new AppUser
+            {
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
+            var result = await _userManager.CreateAsync(user, registerDto.Password);
+            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+
+            return new UserDto
+            {
+                DisplayName = user.DisplayName,
+                Token = "Will be a token here",
+                Email = user.Email,
+            };
+        }
             
 
     }
