9d2389d26af957677eeaaa78f2755588cf3b183d
Taha991 <tahamohamedman@gmail.com>
2023-05-19 15:30:31 +0200

    Seeding Identity Data

---
+++ Infrastructure/Identity/AppIdentityDbContextSeed.cs
@@ -0,0 +1,36 @@
+﻿using Core.Identity;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Identity
+{
+    public class AppIdentityDbContextSeed
+    {
+        public static async Task SeedUserAsync(UserManager<AppUser> userManager)
+        {
+            if(!userManager.Users.Any())
+            {
+                var user = new AppUser
+                {
+                    DisplayName = "Taha",
+                    Email = "tahamohamedman@gmail.com",
+                    UserName = "Taha",
+                    Address = new Address
+                    {
+                        FirstName = "Taha",
+                        LastName = "Mohamed",
+                        Street = "Oubour",
+                        City = "Cairo",
+                        State = "EG",
+                        ZipCode = "11828"
+                    }
+                };
+                await userManager.CreateAsync(user , "P@ssw0rd");
+            }
+        }
+    }
+}
