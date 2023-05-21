52375b1e0afa05bcc7272bc1eecd4e3b4c8eb33c
Taha991 <tahamohamedman@gmail.com>
2023-05-21 13:41:21 +0200

    Creating the account service methods

--- Client/src/app/account/account.service.ts
+++ Client/src/app/account/account.service.ts
@@ -1,9 +1,56 @@
-import { Injectable } from '@angular/core';
+import { Injectable  , OnInit} from '@angular/core';
+import { User } from '../shared/models/user';
+import { BehaviorSubject, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import {  Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AccountService {
+export class AccountService implements OnInit {
+  baseUrl = 'https://localhost:7275/api/';
 
-  constructor() { }
+  private currentUserSource = new BehaviorSubject<User|null>(null);
+  currentUserSource$= this.currentUserSource.asObservable();
+
+
+  constructor(private http:HttpClient, private router:Router) { }
+
+  ngOnInit(): void {
+  }
+
+  register(value:any)
+  {
+    return this.http.post<User>(this.baseUrl+'account/register',value).pipe(
+      map(user => {
+        localStorage.setItem('token' , user.token);
+        this.currentUserSource.next(user);
+
+      })
+    )
+  }
+
+  login(value:any)
+  {
+    return this.http.post<User>(this.baseUrl+'account/login',value).pipe(
+      map(user => {
+        localStorage.setItem('token' , user.token);
+        this.currentUserSource.next(user);
+
+      })
+    )
+  }
+
+  logout()
+  {
+    localStorage.removeItem('token');
+    this.currentUserSource.next(null);
+    this.router.navigateByUrl('/');
+  }
+
+  checkEmailExists(email:string)
+  {
+    return this.http.get<boolean>(this.baseUrl+'account/emailExists?email='+email);
+  }
+
 }
