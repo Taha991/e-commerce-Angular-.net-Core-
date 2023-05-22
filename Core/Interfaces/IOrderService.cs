42c77400590c77fa6ffbcff00d6dfebb9355b1f7
Taha991 <tahamohamedman@gmail.com>
2023-05-22 17:53:34 +0200

    Creating the order controller

--- Core/Interfaces/IOrderService.cs
+++ Core/Interfaces/IOrderService.cs
@@ -1,12 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Core.Entities.OrderAggregate;
 
 namespace Core.Interfaces
 {
-    internal interface IOrderService
+    public interface IOrderService
     {
+        Task<Order> CreateOrderAsync(string buyerEmail, int delieveryMethod, string basketId, Address shippingAddress);
+        Task<IReadOnlyList<Order>> GetOrdersForUserAsync(string buyerEmail);
+        Task<Order> GetOrderByIdAsync(int id, string buyerEmail);
+        Task<IReadOnlyList<DeliveryMethod>> GetDeliveryMethodsAsync();
     }
 }
