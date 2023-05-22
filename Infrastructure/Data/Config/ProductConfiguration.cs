9a2e4908dbb832c5d98d7bda2be01c031dd71bd7
Taha991 <tahamohamedman@gmail.com>
2023-05-22 17:32:51 +0200

    Configuring the order entities

--- Infrastructure/Data/Config/ProductConfiguration.cs
+++ Infrastructure/Data/Config/ProductConfiguration.cs
@@ -1,11 +1,6 @@
 ﻿using Core.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Infrastructure.Data.Config
 {
@@ -13,16 +8,15 @@ namespace Infrastructure.Data.Config
     {
         public void Configure(EntityTypeBuilder<Product> builder)
         {
-
             builder.Property(p => p.Id).IsRequired();
             builder.Property(p => p.Name).IsRequired().HasMaxLength(100);
-            builder.Property(p=> p.Description).IsRequired();
-            builder.Property(p => p.Price).HasColumnType("decimal");
+            builder.Property(p => p.Description).IsRequired();
+            builder.Property(p => p.Price).HasColumnType("decimal(18,2)");
             builder.Property(p => p.PictureUrl).IsRequired();
-            builder.HasOne(p => p.ProductBrand).WithMany().HasForeignKey( p => p.ProductBrandId);
-            builder.HasOne(p=> p.ProductType).WithMany().HasForeignKey(p=> p.ProductTypeId);
-
-
+            builder.HasOne(p => p.ProductBrand).WithMany()
+                .HasForeignKey(p => p.ProductBrandId);
+            builder.HasOne(p => p.ProductType).WithMany()
+                .HasForeignKey(p => p.ProductTypeId);
         }
     }
 }
