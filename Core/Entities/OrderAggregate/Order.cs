be2c6bcb2086a656cde6799906900feb8bc15543
Taha991 <tahamohamedman@gmail.com>
2023-05-22 17:25:11 +0200

    Creating the order aggregate

---
+++ Core/Entities/OrderAggregate/Order.cs
@@ -0,0 +1,33 @@
+﻿namespace Core.Entities.OrderAggregate
+{
+    public class Order : BaseEntity
+    {
+        public Order()
+        {
+        }
+        public Order(IReadOnlyList<OrderItem> orderItems, string buyerEmail, Address shipToAddress,
+            DeliveryMethod deliveryMethod, decimal subtotal, string paymentIntentId)
+        {
+            BuyerEmail = buyerEmail;
+            ShipToAddress = shipToAddress;
+            DeliveryMethod = deliveryMethod;
+            OrderItems = orderItems;
+            Subtotal = subtotal;
+            PaymentIntentId = paymentIntentId;
+        }
+
+        public string BuyerEmail { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public Address ShipToAddress { get; set; }
+        public DeliveryMethod DeliveryMethod { get; set; }
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal Subtotal { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public string PaymentIntentId { get; set; }
+
+        public decimal GetTotal()
+        {
+            return Subtotal + DeliveryMethod.Price;
+        }
+    }
+}
---
+++ Core/Entities/OrderAggregate/ProductItemOrdered.cs
@@ -0,0 +1,6 @@
+﻿namespace Core.Entities.OrderAggregate
+{
+    public class ProductItemOrdered
+    {
+    }
+}
