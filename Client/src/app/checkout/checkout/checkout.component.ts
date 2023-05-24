b380cdd2b35f1a78f303943e486d8f33824c9a2a
Taha991 <tahamohamedman@gmail.com>
2023-05-24 12:34:31 +0200

    Adding the checkout form

--- Client/src/app/checkout/checkout/checkout.component.ts
+++ Client/src/app/checkout/checkout/checkout.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account/account.service';
+import { BasketService } from '../basket/basket.service';
 
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.scss']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+
+  constructor(private fb: FormBuilder, private accountService: AccountService,
+    private basketService: BasketService) { }
+
+  ngOnInit(): void {
+    this.getAddressFormValues();
+    this.getDeliveryMethodValue();
+  }
+
+  checkoutForm = this.fb.group({
+    addressForm: this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      street: ['', Validators.required],
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      zipcode: ['', Validators.required],
+    }),
+    deliveryForm: this.fb.group({
+      deliveryMethod: ['', Validators.required]
+    }),
+    paymentForm: this.fb.group({
+      nameOnCard: ['', Validators.required]
+    })
+  })
+
+  getAddressFormValues() {
+    this.accountService.getUserAddress().subscribe({
+      next: address => {
+        address && this.checkoutForm.get('addressForm')?.patchValue(address)
+      }
+    })
+  }
+
+  getDeliveryMethodValue() {
+    const basket = this.basketService.getCurrentBasketValue();
+    if (basket && basket.deliveryMethodId) {
+      this.checkoutForm.get('deliveryForm')?.get('deliveryMethod')
+        ?.patchValue(basket.deliveryMethodId.toString());
+    }
+  }
 
 }
