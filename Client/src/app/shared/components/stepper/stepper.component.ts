2ac0fc5a0a5e7283cfca9631d01f4027390a0659
Taha991 <tahamohamedman@gmail.com>
2023-05-24 12:24:15 +0200

    Creating a stepper component

---
+++ Client/src/app/shared/components/stepper/stepper.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stepper',
+  templateUrl: './stepper.component.html',
+  styleUrls: ['./stepper.component.scss']
+})
+export class StepperComponent {
+
+}
--- Client/src/app/shared/shared.module.ts
+++ Client/src/app/shared/shared.module.ts
@@ -8,12 +8,15 @@ import { PagerComponent } from './pager/pager.component';
 import {CarouselModule} from 'ngx-bootstrap/carousel';
 import { OrderTotalsComponent } from './order-totals/order-totals.component'
 import { ReactiveFormsModule } from '@angular/forms';
-import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
+import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import { StepperComponent } from './components/stepper/stepper.component'
+import {CdkStepperModule} from '@angular/cdk/stepper'
 @NgModule({
   declarations: [
     PagingHeaderComponent,
     PagerComponent,
-    OrderTotalsComponent
+    OrderTotalsComponent,
+    StepperComponent
   ],
   imports: [
     CommonModule,
@@ -21,7 +24,9 @@ import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
     PaginationModule.forRoot(),
     CarouselModule.forRoot(),
     ReactiveFormsModule,
-    BsDropdownModule.forRoot()
+    BsDropdownModule.forRoot(),
+    CdkStepperModule
+
   ],
   exports:[PaginationModule,
     PagingHeaderComponent,
@@ -29,7 +34,9 @@ import {BsDropdownModule} from 'ngx-bootstrap/dropdown'
     CarouselModule,
     OrderTotalsComponent,
     ReactiveFormsModule,
-    BsDropdownModule
+    BsDropdownModule,
+    StepperComponent
+
   ]
 })
 export class SharedModule { }
