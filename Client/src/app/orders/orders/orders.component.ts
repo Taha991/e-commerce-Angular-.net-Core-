d2de22a4e861096ce3b9162c2b6580ec6dab6a49
Taha991 <tahamohamedman@gmail.com>
2023-05-26 05:27:42 +0200

    Creating the orders list for the user

--- Client/src/app/orders/orders/orders.component.ts
+++ Client/src/app/orders/orders/orders.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '../orders.service';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
   styleUrls: ['./orders.component.scss']
 })
-export class OrdersComponent {
+export class OrdersComponent implements OnInit {
+  orders: Order[] = [];
 
+  constructor(private orderService: OrdersService) { }
+
+  ngOnInit(): void {
+    this.getOrders();
+  }
+
+  getOrders() {
+    this.orderService.getOrdersForUser().subscribe({
+      next: orders => this.orders = orders
+    })
+  }
 }
