3f0d38df9ad67f1b03a201b1ff64b832e24961fd
Taha991 <tahamohamedman@gmail.com>
2023-05-26 05:32:07 +0200

    Making the order details view after submit order

--- Client/src/app/checkout/checkout-success/checkout-success.component.ts
+++ Client/src/app/checkout/checkout-success/checkout-success.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Order } from 'src/app/shared/models/order';
 
 @Component({
   selector: 'app-checkout-success',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./checkout-success.component.scss']
 })
 export class CheckoutSuccessComponent {
+  order?: Order;
 
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.order = navigation?.extras?.state as Order
+  }
 }
--- Client/src/app/orders/order-detailed/order-detailed.component.ts
+++ Client/src/app/orders/order-detailed/order-detailed.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Order } from 'src/app/shared/models/order';
+import { BreadcrumbService } from 'xng-breadcrumb';
+import { OrdersService } from '../orders.service';
 
 @Component({
   selector: 'app-order-detailed',
   templateUrl: './order-detailed.component.html',
   styleUrls: ['./order-detailed.component.scss']
 })
-export class OrderDetailedComponent {
+export class OrderDetailedComponent implements OnInit {
+  order?: Order;
+  constructor(private orderService: OrdersService, private route: ActivatedRoute,
+    private bcService: BreadcrumbService) {
+      this.bcService.set('@OrderDetailed', ' ');
+  }
 
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    id && this.orderService.getOrderDetails(+id).subscribe({
+      next: order => {
+        this.order = order;
+        this.bcService.set('@OrderDetailed', `Order# ${order.id} - ${order.status}`);
+      }
+    })
+  }
 }
