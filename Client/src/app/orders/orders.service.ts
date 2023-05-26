bde9a056b95c2b9c098393f0fbc40c237a4ee9ef
Taha991 <tahamohamedman@gmail.com>
2023-05-26 05:21:53 +0200

    Adding get orders methods in order service

--- Client/src/app/orders/orders.service.ts
+++ Client/src/app/orders/orders.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Order } from '../shared/models/order';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
+ baseUrl= 'https://localhost:7275/api/';
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+
+  getOrdersForUser()
+  {
+    return this.http.get<Order[]>(this.baseUrl+'orders');
+  }
+  getOrderDetails(id:number)
+  {
+    return this.http.get<Order>(this.baseUrl+'orders/'+id);
+  }
 }
